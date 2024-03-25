397ee2eb3609035c50d17be692f6700f41ed8421
Dhvanik Patel <pateldhvanik789@gmail.com>
2024-03-25 19:56:10 +0530

    Login and signup functionality using jsonwebtoken and cookie-parser

--- model/user.js
+++ model/user.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
    username: { type: String, required: true, unique: [true, 'Username already in use!'] },
-   email: { type: String, required: true},
-   password: { type: String, required: true, minLength: [4, 'Too short!'], maxLength: [10, 'Very long!'] },
+   email: { type: String, required: true },
+   password: { type: String, required: true, minLength: [4, 'Too short!'] },
    height: { type: Number, required: true },
    weight: { type: Number, required: true },
    bloodGroup: { type: String, required: true },
